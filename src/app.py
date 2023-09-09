4232676d14ca0feed7cf19c9162f4718bcd427a3
begonufer <begonufernandez@gmail.com>
2023-09-09 10:06:48 +0000

    rutas

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Favorite, Planets, Characters
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +35,79 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify([user.serialize() for user in User.query.all()]),200
 
-    return jsonify(response_body), 200
+@app.route('/user/favorites', methods=['GET'])
+def get_favorites():
+
+    return jsonify([favorites.serialize() for favorites in Favorite.query.all()]),200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    return jsonify([planets.serialize() for planets in Planets.query.all()]),200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    
+    one_planet = Planets.query.get(planet_id)
+
+    return jsonify([one_planet.serialize()]), 200
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+
+    return jsonify([characters.serialize() for characters in Characters.query.all()]),200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_one_character(character_id):
+
+    one_character = Characters.query.get(character_id)
+
+    return jsonify([one_character.serialize()]), 200
+
+@app.route('/user/favorites/planet', methods=['POST'])
+def new_planet_favorite():
+
+    favorite = Favorite()
+    favorite.user_id = request.json.get("user_id")
+    favorite.planet_id = request.json.get("planet_id", None)
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify(favorite.serialize()),200
+
+@app.route('/user/favorites/character', methods=['POST'])
+def new_character_favorite():
+
+    favorite = Favorite()
+    favorite.user_id = request.json.get("user_id")
+    favorite.character_id = request.json.get("character_id", None)
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify(favorite.serialize()),200
+
+@app.route('/user/favorites/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_favorite(planet_id):
+    
+    favorite = Favorite.query.filter_by(planet_id=planet_id).first()
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Planeta eliminado de la lista de favoritos correctamente"}),200  
+
+@app.route('/user/favorites/character/<int:character_id>', methods=['DELETE'])
+def delete_character_favorite(character_id):
+    
+    favorite = Favorite.query.filter_by(character_id=character_id).first()
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Personaje eliminado de la lista de favoritos correctamente"}),200 
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,82 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    firstname = db.Column(db.String, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'{self.id}'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+
+    def __repr__(self):
+        return f'{self.id}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id
+        }
+class Planets(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    terrain = db.Column(db.String)
+    climate = db.Column(db.String)
+    population = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'{self.terrain}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter
+        }
+class Characters(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    birth_year = db.Column (db.Integer)
+    gender = db.Column (db.String)
+    height = db.Column (db.Integer)
+    hair_color = db.Column (db.String)
+    skin_color = db.Column (db.String)
+    eye_color = db.Column (db.String)
+
+    def __repr__(self):
+        return f'{self.birth_year}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color
         }
